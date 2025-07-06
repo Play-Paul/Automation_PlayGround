b7722638f47c491ed23c8ad698ff27c737de0673
Play-Paul <info.playpro@gmail.com>
2025-07-07 00:54:20 +0100

    first code

--- Automation_PlayGround.py
+++ Automation_PlayGround.py
@@ -19,13 +19,16 @@ driver.find_element(By.ID, "submit-id").click()
 time.sleep(3)
 
 #New Customer
-driver.find_element()
-
-#Fill new customer form
-driver.find_element()
-
-#Sign Out
-driver.find_element()
-#Quit browser
+driver.find_element(By.ID, "new-customer").click()
+driver.find_element(By.ID, "EmailAddress").send_keys("info.playprostudios@gmail.com")
+driver.find_element(By.ID, "FirstName").send_keys("Emeka")
+driver.find_element(By.ID, "LastName").send_keys("Paul")
+driver.find_element(By.ID, "City").send_keys("Eko Ni Show")
+driver.find_element(By.ID, "StateOrRegion").send_keys("Maryland")
+driver.find_element(By.XPATH, "//input[@value='male']").click()
+driver.find_element(By.XPATH, "//input[@name='promos-name']").click()
+driver.find_element(By.XPATH, "//button[normalize-space()='Submit']").click()
+driver.find_element(By.CSS_SELECTOR, ".nav-link")
+time.sleep(5)
 driver.quit()
 
